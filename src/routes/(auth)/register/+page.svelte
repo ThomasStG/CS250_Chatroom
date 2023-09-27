0b51dfea1cd1640af7debb2b543ccfa3a79319ca
Thomas Gallaher <ThomasSGallaher@gmail.com>
2023-09-27 13:19:56 -0400

    Update +page.svelte

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -8,12 +8,15 @@
 
 <div class="pt:mt-0 mx-auto flex flex-col items-center justify-center px-6 pb-24 md:h-screen">
   <a href="/" class="mb-8 flex items-center justify-center text-2xl font-semibold lg:mb-10">
+  <a href="/" class="mb-8 flex items-center justify-center text-2xl font-semibold lg:mb-10" style="color:#c34a2c">
     <!-- <img src="/images/logo.svg" class="mr-4 h-11" alt="FlowBite Logo"> -->
     <span>Flowbite</span>
+    <span>[Squire]</span>
   </a>
   <!-- Card -->
   <div class="w-full max-w-xl space-y-8 rounded-md border bg-white p-6 shadow sm:p-8">
     <h2 class="text-2xl font-bold text-gray-900">Create a new account</h2>
+    <h2 class="text-2xl font-bold text-green-900" style="color:#c34a2c">[create_new_account]</h2>
 
     {#if form?.error?.message}
       <Alert type="danger" description={form.error.message} />
@@ -22,6 +25,7 @@
     <form action="?/register" method="POST" use:enhance class="mt-8 space-y-6">
       <div>
         <label for="email" class="mb-2 block text-sm font-medium text-gray-900"> Email </label>
+        <label for="[email]" class="mb-2 block text-sm font-bold text-purple-900" style="color:#c34a2c"> [email] </label>
         <input
           id="email"
           name="email"
@@ -29,21 +33,13 @@
           autocomplete="email"
           required
           class="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label for="username" class="mb-2 block text-sm font-medium text-gray-900"> User-Name </label>
-        <input
-          id="username"
-          name="username"
-          type="text"
-          required
-          class="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
+          class="block w-full rounded-md border border-black-300 bg-coral-50 p-2.5 text-purple-900 focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
         />
       </div>
 
       <div>
         <label for="password" class="mb-2 block text-sm font-medium text-gray-900"> Password </label>
+        <label for="password" class="mb-2 block text-sm font-bold text-gray-900"style="color:#c34a2c"> [password] </label>
         <input
           id="password"
           name="password"
@@ -51,12 +47,15 @@
           autocomplete="new-password"
           required
           class="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
+          class="block w-full rounded-md border border-black-300 bg-coral-50 p-2.5 text-purple-900 focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
         />
       </div>
 
       <div>
         <label for="confirm-password" class="mb-2 block text-sm font-medium text-gray-900">
           Confirm password
+        <label for="confirm-password" class="mb-2 block text-sm font-bold text-gray-900"style="color:#c34a2c">
+          [confirm_password]
         </label>
         <input
           id="password-confirmation"
@@ -65,18 +64,23 @@
           autocomplete="new-password"
           required
           class="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
+          class="block w-full rounded-md border border-black-300 bg-coral-50 p-2.5 text-purple-900 focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
         />
       </div>
 
       <button
         type="submit"
         class="w-full rounded-md bg-primary-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto"
+        class="w-full rounded-md bg-red-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto" 
+        style="background-color:#c34a2c"
       >
         Create account
       </button>
 
       <div class="text-sm font-medium text-gray-500">
         Already have an account? <a href="/login" class="text-primary-700 hover:underline">Login</a>
+      <div class="text-sm font-medium text-yellow-500">
+        Already have an account? <a href="/login" class="text-primary-700 hover:underline" style="color:#c34a2c">Login</a>
       </div>
     </form>
   </div>
