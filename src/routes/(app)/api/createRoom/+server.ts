f461632e8ba406eb17b811330b7e772945920295
Jessemorin77 <jessemorin77@gmail.com>
2023-10-27 13:05:02 -0400

    multiple user implementation

---
+++ src/routes/(app)/api/createRoom/+server.ts
@@ -0,0 +1,34 @@
+import prisma from "$lib/database";
+import { resolveCname } from "dns";
+import type { RequestEvent, RequestHandler } from "./$types";
+import { json } from "@sveltejs/kit";
+
+export async function POST({ request }) {
+  const { addedUsers } = await request.json();
+  
+  const usersToConnect = addedUsers.map(id => ({
+        id: id
+    }));
+ 
+
+  const room = await prisma.room.create({
+    data: {
+      name: "New Groupchat", 
+      Chatroom: true,
+      users: {
+        connect: usersToConnect
+      }
+    },
+    include: {
+      users: true
+    }
+  })
+  
+  if(!room){
+    console.error("error creating room")
+  }else{
+    console.log("room created: ", room)
+  }
+
+  return json({ room }, { status: 201 });
+}
--- src/routes/(app)/chatrooms/create/+page.svelte
+++ src/routes/(app)/chatrooms/create/+page.svelte
@@ -1,68 +1,88 @@
-<script lang="ts">
-    import { User } from "carbon-icons-svelte";
+<script >
+let username = "";
+let userNames = [];
+let addedUsers = [];
 
-    let user = null;
-    let username = "";
-    let userNames = [];
+async function SearchUser() {
+  console.log("username-", username)  
+  try{
+      const response = await fetch("/api/searchFriend", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username }),
+      });
 
-    async function addUser() {
-        if (user && user.username) {
-            userNames.push(user.username);
-            console.log("User added: ", user.username);
-        }
-    }
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
 
-    function testFunc() {
-        console.log("tester");
-    }
+      let user = (await response.json()).user;
+      console.log("Returned User: ", user);
+      console.log(user.id);
+      userNames = [...userNames, user.id];
+      console.log(userNames)
+  } catch(error) {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+  }
+};
 
-    async function searchUser() {
-        console.log("button hit");
-        try {
-            const response = await fetch("/api/searchFriend", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ username }),
-            });
+function addUser(name){
+  addedUsers = [...addedUsers, name];
+  console.log("added users: ", addedUsers)
+}
 
-            if (!response.ok) {
-                throw new Error(
-                    "Network response was not ok " + response.statusText
-                );
-            }
 
-            user = (await response.json()).user;
-            console.log("Returned User: ", user);
-            console.log(user.username);
+async function createRoom() {
+  try{
+      const response = await fetch("/api/createRoom", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ addedUsers }),
+      });
 
-            // After user is retrieved, you can call addUser to add the username
-            addUser();
-        } catch (error) {
-            console.error(
-                "There has been a problem with your fetch operation:",
-                error
-            );
-        }
-    }
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+
+      let room = (await response.json()).room;
+      console.log("Returned Room: ", room);
+  } catch(error) {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+  }
+};
+
 </script>
 
 <div>
-    <div>Search UserNames</div>
-    <form action="">
-        <input placeholder="username" bind:value={username} />
-        <button type="submit" on:click={searchUser}>search</button>
-        {#if user}
-            <div>User: {user.username}</div>
-            <input type="hidden" id="name" bind:value={user.id} />
-            <button type="submit">add to groupchat</button>
-        {/if}
-    </form>
-    <form action="?/createChatroom" method="POST">
-        <input placeholder="Chatroom Name" id="chatname" name="chatname" />
-        <input type="hidden" id="users" name="users" bind:value={userNames} />
-        <button type="submit">Create</button>
-    </form>
-    <div />
+  <div>
+    <h>Create Chatroom</h>
+    <input placeholder="User Name" bind:value={username}/>
+    <button type="submit" on:click={SearchUser} >Search</button>
+  </div>
+  <div>
+    {#each userNames as id (id)}
+      <p>{id}</p>
+      <button on:click={() => addUser(id)}>
+        Add to Groupchat
+      </button>
+    {/each}
+  </div>
+  <div>
+
+      <h> Added to groupchat:</h>
+    {#each addedUsers as id (id)}
+      <p>{id}</p>
+      <button on:click={createRoom}>Create Groupchat</button>
+    {/each}
+  </div>
 </div>
